e3ed336bf6068034285f167bfa025079f2bfc1d5
kevin <desk2003@USherbrooke.ca>
2025-03-24 10:23:55 -0400

    Climat Files refactor

--- scripts/Climatic/climat.py
+++ scripts/Climatic/climat.py
@@ -1,14 +1,15 @@
-import plotly.express as px
+import io
+import folium
+
 import pandas as pd
 import numpy as np
 from aphylogeo import utils
 from aphylogeo.params import Params
-from Climatic.climat_data import get_folium_data
 from Climatic.climat_tree import ClimaticTree
+from Climatic.climat_statistics import ClimaticStatistics
 from event_connector import blocked_signals
 from PyQt6.QtWidgets import QFileDialog
 from utils.custom_table import create_sleek_table
-from utils.download_file import download_local_with_fig
 from utils.error_dialog import show_error_dialog
 from utils.my_dumper import update_yaml_param
 
@@ -17,130 +18,8 @@ class Climat:
     def __init__(self, main):
         self.main = main
         self.climaticTree = ClimaticTree(main)
+        self.climaticStatistics = ClimaticStatistics(main)
         self.sleek_table = None
-        self.customConfig ={
-                'modeBarButtonsToRemove': ['toImage', 'autoScale', 'select2d', 'lasso2d'],
-                'displaylogo' : False
-            }
-
-    def load_climate_statistics(self):
-        """
-        Load climate data from a CSV file, update the UI elements with the column names, and switch to the appropriate tab.
-
-        This method reads the climate data from a CSV file (excluding the first column), updates combo boxes
-        for X and Y axis settings with the column names, and switches to the climate data tab.
-
-        Returns:
-            None
-        """        
-        columns = self.data.columns.tolist()
-
-        if columns:
-            columns.pop(0)
-
-        with blocked_signals(self.main.ClimaticChartSettingsAxisX, self.main.ClimaticChartSettingsAxisY):
-            self.main.ClimaticChartSettingsAxisX.clear()
-            self.main.ClimaticChartSettingsAxisX.addItems(columns)
-            self.main.ClimaticChartSettingsAxisY.clear()
-            self.main.ClimaticChartSettingsAxisY.addItems(columns)
-            self.main.ClimaticChartSettingsAxisY.setCurrentIndex(1)
-            self.main.tabWidget2.setCurrentIndex(2)
-
-        self.generate_climate_graph()
-
-    def generate_climate_graph(self):
-        """
-        Generate and display a graph based on the selected X and Y axis data and the chosen plot type.
-
-        This method reads the selected data columns and plot type from the UI, generates the corresponding graph,
-        and displays it in the specified QLabel widget.
-
-        Returns:
-            None
-        """
-        x_data = self.main.ClimaticChartSettingsAxisX.currentText()
-        y_data = self.main.ClimaticChartSettingsAxisY.currentText()
-        plot_type = self.main.PlotTypesCombobox.currentText()
-        
-        self.main.ClimaticChartSettingsAxisX.setEnabled(True)
-        self.main.ClimaticChartSettingsAxisY.setEnabled(True)
-        
-        match plot_type:
-            case "Scatter Plot":
-                self.generate_scatter_plot(x_data, y_data)
-            case "Line Plot":
-                self.generate_line_plot(x_data, y_data)
-            case "Bar Graph":
-                self.generate_bar_graph(x_data, y_data)
-            case "Violin Plot":
-                self.generate_violin_plot(x_data, y_data)
-            case "Pie Plot":
-                self.generate_pie_plot(x_data, y_data)
-            case "Correlation":
-                self.generate_correlation_plot()
-                self.main.ClimaticChartSettingsAxisX.setEnabled(False)
-                self.main.ClimaticChartSettingsAxisY.setEnabled(False)
-                  
-        self.main.climatGraphView.setHtml(self.fig.to_html(include_plotlyjs='cdn', config= self.customConfig))
-
-    def generate_bar_graph(self, x_data, y_data):
-        self.fig = px.bar(
-        data_frame = self.data,
-        x=x_data,
-        y=y_data,
-        hover_name= self.first_column_name
-        )
-
-    def generate_scatter_plot(self, x_data, y_data):
-        self.fig = px.scatter(
-            data_frame = self.data,
-            x = x_data,
-            y = y_data,
-            hover_name = self.first_column_name
-        )
-
-    def generate_line_plot(self, x_data, y_data):
-        self.fig = px.line(
-            data_frame = self.data,
-            x = x_data,
-            y = y_data,
-            hover_data=[self.first_column_name],
-            markers=True
-        )
-
-    def generate_pie_plot(self, x_data, y_data):
-        self.fig = px.pie(
-        data_frame = self.data,
-        names=self.data[self.first_column_name],
-        values=y_data,
-        hover_data=[x_data]
-        )
-
-    def generate_violin_plot(self, x_data, y_data):   
-        self.fig = px.violin(
-            data_frame = self.data,
-            x = x_data,
-            y = y_data,
-            hover_name = self.first_column_name
-        )
-           
-    def generate_correlation_plot(self):
-        matrix_correlation = self.data.iloc[:, 1:].corr(method="spearman").round(2)
-        self.fig = px.imshow(
-            matrix_correlation,
-            aspect="auto"
-        )
-         
-    def download_climate_plot(self):
-        """
-        Download the generated plot.
-
-        This method allows the user to download the currently displayed plot.
-
-        Returns:
-            None
-        """
-        download_local_with_fig(self.fig)
 
     def load_csv_climate_file(self):
         try:
@@ -158,8 +37,8 @@ class Climat:
                 
                 self.data = pd.read_csv(fullFilePath)
                 columns = self.data.columns.tolist()
-                self.first_column_name = self.data.columns[0]
                 self.data[columns[0]] = self.data[columns[0]].str.replace("_", " ")
+                self.climaticStatistics.data = self.data
                 
                 latitude_col = columns[-1]
                 longitude_col = columns[-2]
@@ -259,7 +138,43 @@ class Climat:
             self.main.tabWidget2.setCurrentIndex(0)
         except Exception as e:
             show_error_dialog(f"An unexpected error occurred: {e}", "Error")
+            
+def get_folium_data(lat, long):
+    """
+    Create and display a folium map with given latitude and longitude.
 
+    This method generates a map centered on the mean latitude and longitude of the provided coordinates.
+    It places markers on the map for each coordinate pair and displays the map in a QWebEngineView.
 
-def round_numbers(val, digits=3):
-    return round(val, digits)
+    Args:
+        lat (list): List of latitudes.
+        long (list): List of longitudes.
+    """
+    try:
+        if not lat or not long or len(lat) != len(long):
+            raise ValueError("Latitude and longitude lists must be non-empty and of the same length.")
+
+        lat = [float(x) for x in lat]  # Convert all elements in lat to float
+        long = [float(x) for x in long]  # Convert all elements in long to float
+
+        mean_lat = sum(lat) / len(lat)
+        mean_long = sum(long) / len(long)
+
+        m = folium.Map(location=[mean_lat, mean_long], zoom_start=4, tiles="OpenStreetMap")  # Adjusted zoom level for better visibility
+
+        # Add markers to the map
+        for latitude, longitude in zip(lat, long):
+            folium.Marker([latitude, longitude]).add_to(m)
+
+        # Adjust the map to fit all markers
+        m.fit_bounds([[min(lat), min(long)], [max(lat), max(long)]])
+
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+
+        return data.getvalue().decode()
+
+    except ValueError as e:
+        show_error_dialog(f"Value Error: {e}")
+    except Exception as e:
+        show_error_dialog(f"An unexpected error occurred: {e}")
