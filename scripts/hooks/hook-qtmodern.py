80cf9364dab74cefb68d049c7ce5f1330ec83b9b
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-09-17 18:31:35 -0400

    Update hook-qtmodern.py

--- scripts/hooks/hook-qtmodern.py
+++ scripts/hooks/hook-qtmodern.py
@@ -1,3 +1,8 @@
 from PyInstaller.utils.hooks import collect_data_files
 
 datas = collect_data_files("qtmodern", includes=["**/*.qss"])
+
+hiddenimports = [
+    "qtmodern.styles",
+    "qtmodern.windows",
+]
