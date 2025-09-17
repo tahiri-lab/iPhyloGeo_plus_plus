5fb5d07349e498cd8ccd0c26b03e9d397ad0cb41
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-09-17 17:07:19 -0400

    Update list_files_to_bundle.py

--- exe-generation/list_files_to_bundle.py
+++ exe-generation/list_files_to_bundle.py
@@ -8,7 +8,7 @@ def list_non_python_files(folder_path):
     that do not end with .py or .pyc.
     """
     for root, dirs, files in os.walk(folder_path):
-        #we must exclude certain folders which are not part of iPhyloGeo++ itself
+        #we exclude certain folders which are not part of iPhyloGeo++ itself
         if ".git" in dirs:
             dirs.remove(".git")
         if ".github" in dirs:
@@ -19,12 +19,13 @@ def list_non_python_files(folder_path):
             dirs.remove(".vscode")
         for file in files:
             excluded = False
-            #we must exclude .py scripts since they will be automatically bundled by pyinstaller
-            #we must exclude .pyc caches since they are not necessary to run iPhyloGeo++
+            #we exclude .py scripts since they will be automatically bundled by pyinstaller
+            #we exclude .pyc caches since they are not necessary to run iPhyloGeo++
             if (file.endswith(".py") or file.endswith(".pyc")):
                 excluded = True
-            #we must exclude .DS_Store and .gitignore
-            if (file == ".DS_Store" or file == ".gitignore"):
+            #we exclude .DS_Store, .gitignore and the README since they are not part of iPhyloGeo++ itself
+            #we exclude the .bat files used to launch iPhyloGeo as it will be launched by the EXE instead
+            if (file in [".DS_Store", ".gitignore", "README.md", "start.bat", "start-no-gpu.bat"]):
                 excluded = True
             if not (excluded):
                 full_path = os.path.join(root, file)
