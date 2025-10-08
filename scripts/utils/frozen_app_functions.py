9ad41217d126a775b6e2fe2dcf775657b2390b63
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-10-08 12:17:30 -0400

    Added internal documentation.

--- scripts/utils/frozen_app_functions.py
+++ scripts/utils/frozen_app_functions.py
@@ -1,7 +1,15 @@
+# This file contains functions which were created in order for iPhyloGeo++ to work not only as a live app, but also as a frozen one.
+
 import os
 import sys
 
 def find_utils(filename):
+    """
+    Method that provides the correct pact to a file within the utils directory.
+
+    args:
+        filename (str): the name of the file, including the extension
+    """
     if getattr(sys, 'frozen', False):
         # The application is frozen
         datadir = os.path.join(os.path.dirname(sys.executable), "lib", "utils")
