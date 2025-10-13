7b7f8b6fd252a6da6a177bb4ec84676570ce517b
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-10-13 17:39:14 -0400

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -9,7 +9,7 @@ includefiles = []
 for image_name in to_include:
     includefiles.append((f'..\img\disabled\{image_name}.svg', f'img\disabled\{image_name}.svg'))
 
-build_options = {'packages': [], 'excludes': [], 'include_files':includefiles}
+build_options = {'packages': [], 'excludes': [], 'include_files':includefiles, 'optimize':2}
 
 base = 'gui'
 
@@ -21,5 +21,4 @@ setup(name='iPhyloGeo_plus_plus',
       version = '1.0',
       description = 'Multi-platform Application for Analyzing Phylogenetic Trees with Climatic Parameters',
       options = {'build_exe': build_options},
-      executables = executables,
-      optimize = 2)
+      executables = executables)
