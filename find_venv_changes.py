0069587b3efb97733a3c5fc9af31e8560ec9090d
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-12-19 12:23:40 -0500

    Create find_venv_changes.py
    Script to help find changes in venv

---
+++ find_venv_changes.py
@@ -0,0 +1,26 @@
+import os
+import time
+from datetime import datetime
+
+# The script will look for changes after this timestamp
+datetime_str = "2025-12-19 12:03:00"
+
+datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+check_ti = datetime_obj.timestamp()
+
+def print_if_later(path, check_ti):
+    # Both the variables would contain time
+    # elapsed since EPOCH in float
+    ti_c = os.path.getctime(path)
+    ti_m = os.path.getmtime(path)
+
+    # Converting the time in seconds to a timestamp
+    c_ti = time.ctime(ti_c)
+    m_ti = time.ctime(ti_m)
+
+    if (ti_m >= check_ti):
+        print(f"{path} \
+        was created at {c_ti} and was "
+              f"last modified at {m_ti}")
+
+print_if_later(".venv/.gitignore", check_ti)
